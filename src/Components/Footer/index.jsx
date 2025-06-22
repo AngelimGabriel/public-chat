8638f6eb011ca972995bdb754a91e1371ea14ac5
Gabriel Angelim <gabriel_angelim@outlook.com>
2025-06-22 13:25:47 -0300

    Alteração da versão de 1.0.0 para 1.0.1

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,7 +3,7 @@ import styles from './style.module.css';
 export default function Footer() {
   return (
     <div className={styles.divFooter}>
-      <p>Projeto desenvolvido por Gabriel Angelim - V1.0.0</p>
+      <p>Projeto desenvolvido por Gabriel Angelim - V1.0.1</p>
     </div>
   );
 }
